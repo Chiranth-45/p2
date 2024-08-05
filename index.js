84f47c46f83f6e3485594856079ff059e7d029d7
Chiranth-45 <150377345+Chiranth-45@users.noreply.github.com>
2024-08-05 20:44:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); // for parsing application/json
 
 app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
+app.set("views",path.join(__dirname,"views")); 
 
 app.use(express.static(path.join(__dirname,"public")));
 let posts=[
